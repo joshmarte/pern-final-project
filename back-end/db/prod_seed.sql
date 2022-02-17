3226c9a804baddc2ae84b9fc7a45b7e2cd9682fd
JoshMarte <joshuelm@gmail.com>
2022-02-17 12:50:03 -0500

    backend setup

--- back-end/db/prod_seed.sql
+++ back-end/db/prod_seed.sql
@@ -6,3 +6,8 @@ INSERT INTO test (name) VALUES
 ('Friday'),
 ('Saturday'),
 ('Sunday');
+
+-- heroku pg:psql postgresql-defined-66539 --app jm-cta-be
+-- \i ./back-end/db/prod_schema.sql
+-- \i ./back-end/db/prod_seed.sql
+-- https://jm-cta-be.herokuapp.com/
